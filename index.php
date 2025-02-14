717e2cfeafb92f33dbcc4212fd77684a33d81002
stan <moon_saga@yahoo.com.tw>
2025-02-14 10:32:38 +0800

    index.php create

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo "I like pizza <br>";
+echo "it is really good <br>";
+//this is a comment
+/* this is a multiline comment*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <button>order a pizza</button>
+
+</body>
+
+</html>

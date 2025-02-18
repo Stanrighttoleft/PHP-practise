2f68305a90a88a0f04cac57f708bed22362046a8
stan <moon_saga@yahoo.com.tw>
2025-02-18 15:13:51 +0800

    connect to server, database.php

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_server = "******";
+$db_user = "*****";
+$db_pass = "********";
+$db_name = "******";
+$conn = "";
+
+try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+} catch (mysqli_sql_exception) {
+    echo "could not connected!";
+}
+
+
+if ($conn) {
+    echo "you are connected";
+}
